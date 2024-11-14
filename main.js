e06f925a3f15f2eb61ac10a298452d65a219ab7b
Unkn0wn-D3v <kimc1145@oshkosh.k12.wi.us>
2024-11-14 10:48:17 -0600

    Update

--- main.js
+++ main.js
@@ -6,17 +6,42 @@ const displayThree = document.getElementById("3");
 const betInput = document.getElementById("bet");
 const balanceText = document.getElementById("kimbucks");
 const clearButton = document.getElementById("clearData");
+const maxBalanceText = document.getElementById("max-balance");
 const slotOptions = [
-  "1", "2", "3", "4", "4", "5", "6", "7", "8", "9", "G",
+  "1",
+  "2",
+  "3",
+  "4",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "1",
+  "2",
+  "3",
+  "4",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "G",
 ];
 
 //Variables
-var spinResult;
+var spinResult1;
+var spinResult2;
+var spinResult3;
 var balance = localStorage.getItem("balance") ?? 100;
 var isSpinning = false;
+var highestBalance = localStorage.getItem("highestBalance") ?? 100;
 
 //Setting defaults
 setBalanceText();
+setMaxBalanceText();
 
 //Button functionality
 clearButton.addEventListener("click", () => {
@@ -68,86 +93,47 @@ spinButton.addEventListener("click", async () => {
     await sleep(2000);
 
     clearInterval(interval1);
-    spinResult = displayOne.textContent;
+    if (displayOne.textContent != "G")
+      spinResult1 = Number(displayOne.textContent);
 
     await sleep(1000);
 
     clearInterval(interval2);
-    spinResult = spinResult + displayTwo.textContent;
+    if (displayTwo.textContent != "G")
+      spinResult2 = Number(displayTwo.textContent);
 
     await sleep(1000);
 
     clearInterval(interval3);
-    spinResult = spinResult + displayThree.textContent;
+    if (displayThree.textContent != "G")
+      spinResult3 = Number(displayThree.textContent);
 
     await sleep(20);
 
-    isSpinning = false;
-
     var reward;
-    var lost;
 
-    switch (spinResult) {
-      case "111":
-        lost = false;
-        reward = bet * 10;
-        window.alert("WOW! 10x");
-        break;
-      case "222":
-        lost = false;
-        reward = bet * 2;
-        window.alert("Wow, 2x!");
-        break;
-      case "333":
-        lost = false;
-        reward = bet * 3;
-        window.alert("Wow, 3x!");
-        break;
-      case "444":
-        lost = false;
-        reward = bet * 4;
-        window.alert("Wow, 4x!");
-        break;
-      case "555":
-        lost = false;
-        reward = bet * 5;
-        window.alert("Wow, 5x!");
-        break;
-      case "666":
-        lost = false;
-        reward = bet * 6;
-        window.alert("Wow, 6x!");
-        break;
-      case "777":
-        lost = false;
-        reward = bet * 7;
-        window.alert("Wow, 7x!");
-        break;
-      case "888":
-        lost = false;
-        reward = bet * 8;
-        window.alert("Wow! 8x!");
-        break;
-      case "999":
-        lost = false;
-        reward = bet * 9;
-        window.alert("WOW, 9x!");
-        break;
-      case "GGG":
-        lost = false;
-        reward = bet * 20;
-        window.alert("WOW OMG JACKPOT, 20x!");
-        break;
-      default:
-        lost = true;
-        reward = bet / 2;
-        window.alert("You lost half your bet :(");
+    if (spinResult1 + 1 == spinResult2 && spinResult2 + 1 == spinResult3) {
+      reward = bet * 3;
+    } else if (spinResult1 == spinResult2 && spinResult2 == spinResult3) {
+      reward = bet * 4;
+    } else if (
+      spinResult1 == spinResult2 ||
+      spinResult1 == spinResult3 ||
+      spinResult2 == spinResult3
+    ) {
+      reward = bet * 2;
+    } else {
+      reward = bet * 0;
     }
-
     reward = Math.floor(reward);
 
     balance += reward;
     setBalanceText();
+
+    if (balance > highestBalance) {
+      highestBalance = balance;
+      setMaxBalanceText();
+    }
 
     if (balance < 1) {
       window.alert(
@@ -156,12 +142,15 @@ spinButton.addEventListener("click", async () => {
 
       clearAllData();
     }
+
+    isSpinning = false;
   }
 });
 
 //On Unload
 window.addEventListener("beforeunload", () => {
   localStorage.setItem("balance", balance);
+  localStorage.setItem("highestBalance", highestBalance);
 });
 
 //Functions
@@ -176,6 +165,10 @@ function getRandomInt(max) {
 
 function setBalanceText() {
   balanceText.textContent = balance + " KB";
+}
+
+function setMaxBalanceText() {
+  maxBalanceText.textContent = "Highest Balance: " + highestBalance;
 }
 
 function clearAllData() {
